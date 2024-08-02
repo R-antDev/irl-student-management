b598ad65c99338b6c2e06d419b2ffbb38f293a1e
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-08-03 00:58:06 +0600

    update edit added

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Classes;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Resources\ClassResource;
+use App\Http\Resources\StudentResource;
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
+
+
+class StudentController extends Controller
+{
+    //
+    public function index(){
+        $students = Student::paginate(10);
+
+        return Inertia::render('Student/Index', [
+            'students' => StudentResource::collection($students)
+        ]);
+    }
+
+    public function create(){
+        $classes = ClassResource::collection(Classes::all());
+        return Inertia::render('Student/Create', [
+            'classes' => $classes
+        ]);
+    }
+
+    public function store(StoreStudentRequest $request){
+        Student::create($request->validated());
+        return redirect()->route('students.index');
+    }
+
+    public function edit(Student $student){
+        $classes = ClassResource::collection(Classes::all());
+        return Inertia::render('Student/Edit', [
+            'classes' => $classes,
+            'student' => StudentResource::make($student)
+        ]);
+    }
+
+    public function update(UpdateStudentRequest $request, Student $student){
+        $student->update($request->validated());
+        return redirect()->route('students.index');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SectionController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -22,6 +24,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::resource('students', StudentController::class);
+    Route::resource('sections', SectionController::class)->only(['index']);
 });
 
 require __DIR__.'/auth.php';
